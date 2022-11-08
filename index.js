6c6ed33e3488cae46ef2266a451ceb3be49508da
asadaman42 <asadaman42@gmail.com>
2022-11-08 11:05:51 +0600

    server created

---
+++ index.js
@@ -0,0 +1,22 @@
+// required
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+/*************** 
+    Operation
+***************/
+
+app.get('/', (req, res) => {
+    res.send('Server is okay.')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port - ${port}`)
+})
+
